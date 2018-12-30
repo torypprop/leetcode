6a08c84f74567f15d19f1a9c8521526512647fc8
torypprop <torypprop@gmail.com>
2018-12-30 08:51:36 +0800

    second commit

---
+++ src/leetcode/_051_100/SearchinRotatedSortedArrayII_81.java
@@ -0,0 +1,21 @@
+package leetcode._051_100;
+
+public class SearchinRotatedSortedArrayII_81 {
+
+    public boolean search(int[] nums, int target) {
+    	int left = 0, right = nums.length - 1;
+	    while (left <= right) {
+	        int mid = (left + right) / 2;
+	        if (target == nums[mid]) return true;
+	        if (nums[mid] == nums[left]) left++;		//´Ë´¦¹Ø¼ü
+	        else if (nums[mid] > nums[left]) {
+	            if (nums[left] <= target && nums[mid] > target) right = mid - 1;
+	            else left = mid + 1;
+	        } else {
+	            if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+	            else right = mid - 1;
+	        }
+	    }
+	    return false;
+    }
+}
