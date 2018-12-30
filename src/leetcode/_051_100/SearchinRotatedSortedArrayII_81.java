d0be663609ef575867bd87ef4f1b00561eedc7da
torypprop <torypprop@gmail.com>
2018-12-30 08:58:55 +0800

    commit

--- src/leetcode/_051_100/SearchinRotatedSortedArrayII_81.java
+++ src/leetcode/_051_100/SearchinRotatedSortedArrayII_81.java
@@ -6,7 +6,7 @@ public class SearchinRotatedSortedArrayII_81 {
     	int left = 0, right = nums.length - 1;
 	    while (left <= right) {
 	        int mid = (left + right) / 2;
-	        if (target == nums[mid]) return true;
+	        if (target == nums[mid]) return true;		//先判定是否相等
 	        if (nums[mid] == nums[left]) left++;		//此处关键,因为left可能和mid相等
 	        else if (nums[mid] > nums[left]) {
 	            if (nums[left] <= target && nums[mid] > target) right = mid - 1;
